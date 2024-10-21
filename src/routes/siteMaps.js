e896d6bb997de0c48bb44f866d537350c3acc6dc
FivesLuis <agomez@fivesconsulting.com>
2024-10-21 13:55:24 -0600

    permisos por usuario

--- src/routes/siteMaps.js
+++ src/routes/siteMaps.js
@@ -7,26 +7,16 @@ import {
   faStore,
   faUsers,
   faUserSecret,
-  faBox
+  faBox,
 } from "@fortawesome/free-solid-svg-icons";
 
-export const dashboardRoutes = {
-  label: "Dashboard",
-  labelDisable: true,
-  children: [
-    {
-      name: 'Inicio',
-      to: '/',
-      exact: true,
-      active: true
-    },
-  ], 
-};
+const user = JSON.parse(localStorage.getItem("user"));
 
-export const appRoutes = {
+// Definir una versión alternativa de appRoutes si user es 1
+const altAppRoutes = {
   label: "Modulos",
   children: [
-    {
+    /*{
       name: "Catalogos",
       icon: faBook,
       active: true,
@@ -74,7 +64,7 @@ export const appRoutes = {
           active: true,
         },
       ],
-    },
+    },*/
     {
       name: "Comercial",
       icon: "dollar-sign",
@@ -86,30 +76,12 @@ export const appRoutes = {
           to: "/comercial/reservas",
           active: true,
         },
-        {
+        /*{
           name: "Paqueteria",
           icon: faBox,
           to: "/comercial/paqueteria",
           active: true,
-        },
-      ],
-    },
-    {
-      name: "Logistica",
-      icon: "dollar-sign",
-      active: true,
-      children: [
-        {
-          name: "Rutas",
-          icon: faStore,
-          to: "/logistica/rutas",
-          active: true,
-        },{
-          name: "Pasajeros",
-          icon: faUsers,
-          to: "/app/kanban",
-          active: true,
-        },
+        },*/
       ],
     },
     {
@@ -127,6 +99,116 @@ export const appRoutes = {
     },
   ],
 };
+
+// Condicionalmente asignar appRoutes
+export const appRoutes =
+  user.ID === 1
+    ? altAppRoutes
+    : {
+        label: "Modulos",
+        children: [
+          {
+            name: "Catalogos",
+            icon: faBook,
+            active: true,
+            children: [
+              {
+                name: "Usuarios",
+                icon: faUser,
+                to: "/configuration/users",
+                active: true,
+              },
+              {
+                name: "Agentes",
+                icon: faUserSecret,
+                to: "/configuration/agentes",
+                active: true,
+              },
+              {
+                name: "Vehiculos",
+                icon: faTruck,
+                to: "/configuration/vehiculos",
+                active: true,
+              },
+              {
+                name: "Rutas",
+                icon: faRoute,
+                to: "/configuration/rutas",
+                active: true,
+              },
+              {
+                name: "Destinos",
+                icon: faSearchLocation,
+                to: "/configuration/destinos",
+                active: true,
+              },
+              {
+                name: "Perfiles",
+                icon: faSearchLocation,
+                to: "/configuration/profiles",
+                active: true,
+              },
+              {
+                name: "Clientes",
+                icon: faUsers,
+                to: "/catalogo/clientes",
+                active: true,
+              },
+            ],
+          },
+          {
+            name: "Comercial",
+            icon: "dollar-sign",
+            active: true,
+            children: [
+              {
+                name: "Reservas",
+                icon: faStore,
+                to: "/comercial/reservas",
+                active: true,
+              },
+              {
+                name: "Paqueteria",
+                icon: faBox,
+                to: "/comercial/paqueteria",
+                active: true,
+              },
+            ],
+          },
+          {
+            name: "Logistica",
+            icon: "dollar-sign",
+            active: true,
+            children: [
+              {
+                name: "Rutas",
+                icon: faStore,
+                to: "/logistica/rutas",
+                active: true,
+              },
+              {
+                name: "Pasajeros",
+                icon: faUsers,
+                to: "/app/kanban",
+                active: true,
+              },
+            ],
+          },
+          {
+            name: "Exploradores",
+            icon: "search",
+            active: true,
+            children: [
+              {
+                name: "Rutas",
+                icon: faRoute,
+                to: "/Explorador/ExploradorRutas",
+                active: true,
+              },
+            ],
+          },
+        ],
+      };
 
 const routes = [dashboardRoutes, appRoutes];
 
